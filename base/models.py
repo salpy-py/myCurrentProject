b7a8c75781a92b8fcd5963dc8ce6ac1dcbe4ab75
salman <salman2711x@gmail.com>
2024-12-07 07:44:46 +0500

    done with database

--- base/models.py
+++ base/models.py
@@ -3,13 +3,13 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 
+
 class Topic(models.Model):
     name = models.CharField(max_length=200)
-    
+
     def __str__(self):
         return self.name
-    
-    
+
 
 class Room(models.Model):
     host = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
@@ -20,8 +20,11 @@ class Room(models.Model):
     updated = models.DateTimeField(auto_now=True)
     created = models.DateTimeField(auto_now_add=True)
 
+    class Meta:
+        ordering = ["-updated", "-created"]
+
     def __str__(self):
-            return self.name
+        return self.name
 
 
 class Message(models.Model):
@@ -30,7 +33,6 @@ class Message(models.Model):
     body = models.TextField()
     updated = models.DateTimeField(auto_now=True)
     created = models.DateTimeField(auto_now_add=True)
-    
+
     def __str__(self):
         return self.body[0:50]
-    
